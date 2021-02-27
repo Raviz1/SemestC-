a496ec79e221abc8afd194117c20927f9eb62e0e
Adrian <adrianscibisz@gmail.com>
2021-02-27 01:47:34 +0100

    Context z bazy

---
+++ Databases/Context.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+/// <summary>
+/// Dziedziczenie z na ORM wraz z inicjalizacją
+/// </summary>
+
+namespace FootballTeam.Databases
+{
+/// <summary>
+///  odziedziczony context
+/// </summary>
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Footballer> Footballers { get; set; }
+        public DbSet<Sponsor> Sponsors { get; set; }
+        public DbSet<SportClub> SportClubs { get; set; }
+        public DbSet<TransferHistory> TransferHistories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+   
+
+}
