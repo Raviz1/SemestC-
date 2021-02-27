40479cf1077cd3677a7eb0d13ce01e6eb351287a
Adrian <adrianscibisz@gmail.com>
2021-02-27 01:51:42 +0100

    Zakonczony projekt

---
+++ App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using FootballTeam.Databases;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FootballTeam
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ServiceProvider serviceProvider;
+
+        public App()
+        {
+            // używamy singletona w aplikacji
+            ServiceCollection services = new ServiceCollection();
+            services.AddDbContext<Context>(option =>
+            {
+                option.UseSqlite("Data Source = FootballTeam.db");
+            });
+
+            services.AddSingleton<MainWindow>();
+            serviceProvider = services.BuildServiceProvider();
+        }
+        // startup
+        private void OnStartup(object s, StartupEventArgs e)
+        {
+            var mainWindow = serviceProvider.GetService<MainWindow>();
+            mainWindow.Show();
+        }
+    }
+}
