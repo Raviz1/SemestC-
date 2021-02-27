2672dc25ca4a5d5719c8f74485ed9e813bc1d8f8
Adrian <adrianscibisz@gmail.com>
2021-02-27 01:41:59 +0100

    Modele adress

---
+++ Databases/Models.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Modele bazy danych
+/// </summary>
+namespace FootballTeam.Databases
+{
+    /// <summary>
+    /// Model adresów
+    /// </summary>
+    public class Address
+    {
+        public int Id { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public string Number { get; set; }
+
+        public string Country { get; set; }
+
+    }
+
+}
